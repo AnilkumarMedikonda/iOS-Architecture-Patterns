aaae4afe4fac6297d76a8d7589e28a1bfe00fcc4
AnilkumarMedikonda <62655943+AnilkumarMedikonda@users.noreply.github.com>
2026-04-05 08:03:50 +0530

    MVCSetupCodedone

---
+++ MVC-UIKit-UserList-App/MVC-UIKit-UserList-App/Resources/Constants/APIConstants.swift
@@ -0,0 +1,22 @@
+//
+//  APIConstants.swift
+//  MVC-UIKit-UserList-App
+//
+//  Created by Medikonda Anil kumar on 05/04/26.
+//
+
+import Foundation
+
+
+struct APIConstants {
+    
+    static let baseURL = "https://jsonplaceholder.typicode.com"
+    
+    enum EndPoints: String {
+        case users = "/users"
+        
+        var url: URL? {
+            return URL(string: APIConstants.baseURL + self.rawValue)
+        }
+    }
+}
