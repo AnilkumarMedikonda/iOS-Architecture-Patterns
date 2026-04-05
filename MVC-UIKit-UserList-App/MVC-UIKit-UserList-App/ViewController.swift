5af56f381b1d082320a285018ca9a3609ff1fcd1
AnilkumarMedikonda <62655943+AnilkumarMedikonda@users.noreply.github.com>
2026-04-05 06:10:02 +0530

    Add MVC UIKit project initial setup

---
+++ MVC-UIKit-UserList-App/MVC-UIKit-UserList-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-UIKit-UserList-App
+//
+//  Created by Medikonda Anil kumar on 05/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVC-UIKit-UserList-App/MVC-UIKit-UserList-AppTests/MVC_UIKit_UserList_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MVC_UIKit_UserList_AppTests.swift
+//  MVC-UIKit-UserList-AppTests
+//
+//  Created by Medikonda Anil kumar on 05/04/26.
+//
+
+import Testing
+@testable import MVC_UIKit_UserList_App
+
+struct MVC_UIKit_UserList_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
